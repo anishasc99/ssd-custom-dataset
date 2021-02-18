c685b586d36451c72e29c5618297a093a12c5324
Anisha Chazhoor <67101520+anishasc99@users.noreply.github.com>
2021-02-18 16:19:22 +0530

    Update config.py

--- data/config.py
+++ data/config.py
@@ -31,7 +31,7 @@ coco = {
     #'num_classes': 201,
     'num_classes': 9,
     'lr_steps': (280000, 360000, 400000),
-    'max_iter': 10000,
+    'max_iter': 40000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
